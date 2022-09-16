a5aa36e7ad9b2e2d638905e9bffd99730e5e9e06
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-16 22:23:13 +0100

    Remove verbose comments

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -28,36 +28,10 @@ class DiaryEntry
 
   def reading_chunk(wpm, minutes)
     words_amount = wpm * minutes
-    # Calculates the amount of words the user can read.
-    # p "words_amount: #{words_amount}"
-    # p "@counter before size check: #{@counter}"
     if @counter > count_words then @counter = 0 end
-    # p "@counter after size check: #{@counter}"
-    # Resets @counter when it is larger than length of @contents.
-    # Allows looping back to the beginning once the last chunk of
-    # @contents is returned.
     start = @counter
-    # p "start: #{start}"
-    # @counter is being changed but the pre-change version of it is part of the return statement
-    # so it is being saved in 'start' for it to be returned.
     stop = words_amount + start
-    # p "stop: #{stop}"
-    # Marks last index for the word array being returned.
     @counter += words_amount
-    # p "Counter after addition of words_amount: #{@counter}"
-    # This modification is important so the next time this method is called
-    # we get a different start position.
     @contents.split[start...stop].join(' ')
-    # Returns a string with a chunk of the contents that the user could read
-    # in the given number of minutes.
-    # If called again, `reading_chunk` should return the next chunk, skipping
-    # what has already been read, until the contents is fully read.
-    # The next call after that it should restart from the beginning.
   end
 end
-
-# my_entry = DiaryEntry.new('monday', 'one space two space three space four space five space six space seven space eight space nine space')
-
-# p my_entry.reading_chunk(2, 2)
-# p my_entry.reading_chunk(2, 2)
-# p my_entry.reading_chunk(2, 2)
