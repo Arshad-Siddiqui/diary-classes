7caba664e8d02695e98f42042a9fb65ee8e8a22f
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-16 22:21:18 +0100

    Create and test DiaryEntry class

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -1,30 +1,53 @@
 class DiaryEntry
-  def initialize(title, contents) # title, contents are strings
-    # ...
+  def initialize(title, contents)
+    @title = title
+    @contents = contents
+    @counter = 0
+    # @counter is an integer used to keep track of the words read.
+    # @contents and @title are strings
   end
 
   def title
-    # Returns the title as a string
+    @title
   end
 
   def contents
-    # Returns the contents as a string
+    @contents
   end
 
   def count_words
     # Returns the number of words in the contents as an integer
+    @contents.split.length
   end
 
-  def reading_time(wpm) # wpm is an integer representing
-                        # the number of words the user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # for the contents at the given wpm.
+  def reading_time(wpm)
+    # Calculates length of time used to read @contents in minutes
+    # based on words per minute and then rounded up.
+    (count_words.to_f / wpm).ceil
   end
 
-  def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
-                                  # of words the user can read per minute
-                                  # `minutes` is an integer representing the
-                                  # number of minutes the user has to read
+  def reading_chunk(wpm, minutes)
+    words_amount = wpm * minutes
+    # Calculates the amount of words the user can read.
+    # p "words_amount: #{words_amount}"
+    # p "@counter before size check: #{@counter}"
+    if @counter > count_words then @counter = 0 end
+    # p "@counter after size check: #{@counter}"
+    # Resets @counter when it is larger than length of @contents.
+    # Allows looping back to the beginning once the last chunk of
+    # @contents is returned.
+    start = @counter
+    # p "start: #{start}"
+    # @counter is being changed but the pre-change version of it is part of the return statement
+    # so it is being saved in 'start' for it to be returned.
+    stop = words_amount + start
+    # p "stop: #{stop}"
+    # Marks last index for the word array being returned.
+    @counter += words_amount
+    # p "Counter after addition of words_amount: #{@counter}"
+    # This modification is important so the next time this method is called
+    # we get a different start position.
+    @contents.split[start...stop].join(' ')
     # Returns a string with a chunk of the contents that the user could read
     # in the given number of minutes.
     # If called again, `reading_chunk` should return the next chunk, skipping
@@ -32,3 +55,9 @@ class DiaryEntry
     # The next call after that it should restart from the beginning.
   end
 end
+
+# my_entry = DiaryEntry.new('monday', 'one space two space three space four space five space six space seven space eight space nine space')
+
+# p my_entry.reading_chunk(2, 2)
+# p my_entry.reading_chunk(2, 2)
+# p my_entry.reading_chunk(2, 2)
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,2 +1,32 @@
 require 'diary_entry'
 require 'diary'
+
+RSpec.describe DiaryEntry do
+  describe '#reading_chunk' do
+    it 'returns first chunk' do
+      my_entry = DiaryEntry.new('monday', 'one space two space three space four space five space')
+      expect(my_entry.reading_chunk(2, 2)).to eq 'one space two space'
+    end
+
+    it 'returns second chunk' do
+      my_entry = DiaryEntry.new('monday', 'one space two space three space four space five space')
+      my_entry.reading_chunk(2, 2)
+      expect(my_entry.reading_chunk(2, 2)).to eq 'three space four space'
+    end
+
+    it "it can return end of string even during overshoot" do
+      my_entry = DiaryEntry.new('monday', 'one space two space three space four space five space')
+      my_entry.reading_chunk(2, 2)
+      my_entry.reading_chunk(2, 2)
+      expect(my_entry.reading_chunk(2, 2)).to eq 'five space'
+    end
+
+    it "loops back to first chunk after returning last chunk" do
+      my_entry = DiaryEntry.new('monday', 'one space two space three space four space five space')
+      my_entry.reading_chunk(2, 2)
+      my_entry.reading_chunk(2, 2)
+      my_entry.reading_chunk(2, 2)
+      expect(my_entry.reading_chunk(2, 2)).to eq 'one space two space'
+    end
+  end
+end
