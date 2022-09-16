40d56b7ea286e9a94a5f0aedd99537b01ec5397c
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-16 22:38:12 +0100

    Refactored code to make shorter

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -3,29 +3,26 @@ class DiaryEntry
     @title = title
     @contents = contents
     @counter = 0
-    # @counter is an integer used to keep track of the words read.
-    # @contents and @title are strings
   end
 
   attr_reader :title, :contents
 
   def count_words
-    # Returns the number of words in the contents as an integer
     @contents.split.length
   end
 
   def reading_time(wpm)
-    # Calculates length of time used to read @contents in minutes
-    # based on words per minute and then rounded up.
     (count_words.to_f / wpm).ceil
   end
 
   def reading_chunk(wpm, minutes)
     words_amount = wpm * minutes
-    if @counter > count_words then @counter = 0 end
+    @counter > count_words && @counter = 0
     start = @counter
     stop = words_amount + start
     @counter += words_amount
     @contents.split[start...stop].join(' ')
+    # Returns chunk excluding previously read chunk unless it
+    # reached the end
   end
 end
